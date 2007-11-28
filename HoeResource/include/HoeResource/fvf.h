7802e0e4a793a9e0e09cc33c2e29a4e4d97a23b9
Gejza'X <gejza@gejza.net>
2007-11-28 00:10:26 +0000

    fvf

--- HoeResource/include/HoeResource/fvf.h
+++ HoeResource/include/HoeResource/fvf.h
@@ -1,1 +1,16 @@
-// fvf!
+
+#ifndef _HOE_RESOURCE_FVF_H_
+#define _HOE_RESOURCE_FVF_H_
+
+namespace HoeRes {
+
+class HoeFVF
+{
+public:
+	// res
+};
+
+} // namespace HoeRes
+
+#endif // _HOE_RESOURCE_FVF_H_
+
